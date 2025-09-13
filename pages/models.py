9dfa7a9971161d4df60461ce238580b23940ee9e
FaroukDEV7 <faroukeldreinydev74@gmail.com>
2025-09-13 06:29:28 +0300

    Add Project and Category models with migrations

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Projects(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/%Y/%m/%d/')
+    funded_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    donatuion_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    donors = models.IntegerField()
+    days_left = models.IntegerField()
+    about_project = models.TextField()
+    category = models.ForeignKey('Categories', on_delete=models.CASCADE, related_name='projects')
+
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+
+class Categories(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
