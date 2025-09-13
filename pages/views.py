bb0aebe9e9a0c99624de98e403f3dc4be0265ee3
FaroukDEV7 <faroukeldreinydev74@gmail.com>
2025-09-13 07:05:41 +0300

    Add projects page and configure URL routing

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from .models import Projects , Categories
 # Create your views here.
+
+# home page
 def index(request):
     context = {
         'projects': Projects.objects.all()[:3],
         'categories': Categories.objects.all()
     }
     return render(request, 'pages/index.html', context)
+
+
+
+# projects page
+def projects(request):
+    context = {
+        'projects': Projects.objects.all(),
+        'categories': Categories.objects.all()
+    }
+    return render(request, 'pages/projects.html', context)
+
