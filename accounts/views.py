1092774d8a82c500885f107ab72ae69a66f10ddd
FaroukDEV7 <faroukeldreinydev74@gmail.com>
2025-09-14 23:03:04 +0300

    Implement user signup functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render , redirect
+from .forms import SignUpForm
+from django.contrib.auth import authenticate , login# Create your views here.
 
 def signup(request):
-    pass
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username , password=password)
+            login(request , user)
+            return redirect('/accounts/profile')
+            
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
--- project/urls.py
+++ project/urls.py
@@ -21,6 +21,7 @@ from django.urls import path , include
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
