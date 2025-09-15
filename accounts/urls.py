f6f7f7f48d64566e07aadff6f76fd82288ecabd7
FaroukDEV7 <faroukeldreinydev74@gmail.com>
2025-09-15 07:00:23 +0300

    Configure password reset to send emails via Gmail

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('profile/',views.profile , name='profile'),
+    path(
+    "reset_password/",
+    auth_views.PasswordResetView.as_view(
+        template_name="registration/password_reset.html",              
+        email_template_name="registration/password_reset_email.html",  
+        html_email_template_name="registration/password_reset_email.html"  
+    ),
+    name="reset_password",
+),
 ]
+
+
